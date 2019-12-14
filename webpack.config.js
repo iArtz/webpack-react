a70646bde2306877ae6a58671bab22a693b7dde4
iArtz <iArtz@me.com>
2019-12-14 16:08:47 +0700

    Added sass loader node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,15 @@ const config = {
           { loader: 'css-loader' },
         ],
       },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
     ],
   },
   plugins: [
